776fd2a91006639eab120c828334735426f663ca
Arjun Bhattad <163147180+ArjunBhattad2004@users.noreply.github.com>
2024-11-15 15:52:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Arjun@2004'
+    MYSQL_PASSWORD = 'password' # Replace with your password
     MYSQL_DB = 'hotel_management'
     SECRET_KEY = os.urandom(24)
